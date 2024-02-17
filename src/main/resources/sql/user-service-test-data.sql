b55896992595b683739b35beeb9830d1b41ee461
2jigoo <2jigoo@naver.com>
2024-02-17 19:36:22 +0900

    1-3. h2를 이용한 service 테스트
    - UserService
    - PostService

---
+++ src/main/resources/sql/user-service-test-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+VALUES (1, '2jigoo@naver.com', '2jigoo', 'Seoul', 'aaaaa-aaaaa-aaaaa-aaaaa', 'ACTIVE', 0);
+
+INSERT INTO `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+VALUES (2, '2jigoo2@naver.com', '2jigoo2', 'Seoul', 'aaaaa-aaaaa-aaaaa-aaaaa', 'PENDING', 0);
